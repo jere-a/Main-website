5ca50b44c12b14e585b58dda7193429a2cfb0ada
Jere Laitinen <85607228+jere-a@users.noreply.github.com>
2024-05-31 21:55:07 +0300

    Updated page

---
+++ src/content/blog/test.md
@@ -0,0 +1,7 @@
+---
+title: 'test'
+tags: ['1', '2']
+pubDate: 2024-12-24
+---
+
+fgdjhsyhvdhfyvgdhngljfdhnvgdfh
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,26 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    title: 'Åzze’s Blog',
+    description: 'A small indebended content creator for the internet',
+    site: context.site,
+    stylesheet: '/styles.xsl',
+    items: blog.map((post) => ({
+        title: post.data.title,
+        tags: post.data.tags,
+        content: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+        }),
+        pubDate: post.data.pubDate,
+        // Compute RSS link from post `slug`
+        // This example assumes all posts are rendered as `/blog/[slug]` routes
+        link: `/blog/${post.slug}/`,
+    })),
+  });
+}
