0a0d6f518945b9b10fc6c088451c43f20a049010
Jere Laitinen <85607228+jere-a@users.noreply.github.com>
2024-07-19 00:59:43 +0300

    Fixed Pull request.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,7 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
-import { siteConfig } from '../config';
-const markdown = new MarkdownIt({
+const parser = new MarkdownIt({
   html: true,
   breaks: true,
   linkify: true,
@@ -12,19 +11,20 @@ const markdown = new MarkdownIt({
 export async function GET(context) {
   const blog = await getCollection('blog');
   return rss({
-    title: 'Åzze’s Blog',
-    description: 'A small indebended content creator for the internet',
-    site: context.site,
-    xmlns: {atom: 'http://www.w3.org/2005/Atom'},
-    items: blog.map((post) => ({
-        title: post.data.title,
-        description: sanitizeHtml(markdown.render(post.body).replace('src="/', `src="${siteConfig.url}/`).replace('href="/', `href="${siteConfig.url}/`)
-                                  .split(' ').slice(0, 50).join(' ')),
-        content: sanitizeHtml(markdown.render(post.body)),
-        pubDate: post.data.pubDate,
-        // Compute RSS link from post `slug`
-        // This example assumes all posts are rendered as `/blog/[slug]` routes
-        link: `/blog/${post.slug}/`,
-    })),
+		title: 'Åzze’s Blog',
+		description: 'A small indebended content creator for the internet',
+		site: context.site,
+		xmlns: { atom: 'http://www.w3.org/2005/Atom' },
+		items: blog.map((post) => ({
+			title: post.data.title,
+			tags: post.data.tags,
+			content: sanitizeHtml(parser.render(post.body), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+			}),
+			pubDate: post.data.pubDate,
+			// Compute RSS link from post `slug`
+			// This example assumes all posts are rendered as `/blog/[slug]` routes
+			link: `/blog/${post.slug}/`,
+		})),
   });
 }
