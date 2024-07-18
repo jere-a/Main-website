4af9729f4ca381ff848e906b32229cfe89dbe783
Jere Laitinen <85607228+jere-a@users.noreply.github.com>
2024-07-19 00:45:22 +0300

    Removed styles.xsl file

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,12 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
-const parser = new MarkdownIt();
+import { siteConfig } from '../config';
+const markdown = new MarkdownIt({
+  html: true,
+  breaks: true,
+  linkify: true,
+});
 
 export async function GET(context) {
   const blog = await getCollection('blog');
@@ -10,13 +15,12 @@ export async function GET(context) {
     title: 'Åzze’s Blog',
     description: 'A small indebended content creator for the internet',
     site: context.site,
-    stylesheet: '/styles.xsl',
+    xmlns: {atom: 'http://www.w3.org/2005/Atom'},
     items: blog.map((post) => ({
         title: post.data.title,
-        tags: post.data.tags,
-        content: sanitizeHtml(parser.render(post.body), {
-          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-        }),
+        description: sanitizeHtml(markdown.render(post.body).replace('src="/', `src="${siteConfig.url}/`).replace('href="/', `href="${siteConfig.url}/`)
+                                  .split(' ').slice(0, 50).join(' ')),
+        content: sanitizeHtml(markdown.render(post.body)),
         pubDate: post.data.pubDate,
         // Compute RSS link from post `slug`
         // This example assumes all posts are rendered as `/blog/[slug]` routes
