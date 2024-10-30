cec1f063e1ab15583ca6bfa08b2127087f597341
Jere Laitinen <85607228+jere-a@users.noreply.github.com>
2024-10-30 23:51:48 +0200

    another fix for nav-menu

--- src/components/react/nav-menu.tsx
+++ src/components/react/nav-menu.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Link from 'next/link';
 
 import '../../global.css';
-import '@/theme.css';
 import '../../styles/animate.css';
 import '../../styles/holidays/index.css';
 
