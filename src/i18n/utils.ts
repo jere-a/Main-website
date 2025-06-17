36eb189e7b827f0c58f6f33f0e3f41b6f1d49dff
Åzze <85607228+jere-a@users.noreply.github.com>
2025-06-17 15:12:39 +0300

    Updating files

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,23 +1,23 @@
 import { ui, defaultLang, showDefaultLang } from './ui';
 
 export function getLangFromUrl(url: URL) {
-	const [, lang] = url.pathname.split('/');
-	if (lang in ui) return lang as keyof typeof ui;
-	return defaultLang;
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
 }
 
 export function useTranslations(lang: string) {
-	return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-	  const validLang = (lang in ui ? lang : defaultLang) as keyof typeof ui;
-	  return ui[validLang][key] || ui[defaultLang][key];
-	};
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    const validLang = (lang in ui ? lang : defaultLang) as keyof typeof ui;
+    return ui[validLang][key] || ui[defaultLang][key];
+  };
 }
 
 export function useTranslatedPath(lang: string) {
-	return function translatePath(path: string, l: string = lang) {
-	  const validLang = (lang in ui ? lang : defaultLang) as keyof typeof ui;
-		return !showDefaultLang && l === defaultLang ? path : `/${validLang}${path}`;
-	};
+  return function translatePath(path: string, l: string = lang) {
+    const validLang = (lang in ui ? lang : defaultLang) as keyof typeof ui;
+    return !showDefaultLang && l === defaultLang ? path : `/${validLang}${path}`;
+  };
 }
 
 export function iterator(data: any) {
