738ba0eeb8d902bd75311cbd733414b769537e03
Åzze <85607228+jere-a@users.noreply.github.com>
2025-09-27 01:34:40 +0300

    changes just the robots.txt in this

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,11 +1,6 @@
 import type { APIRoute } from "astro";
 
 const getRobotsTxt = (sitemapURL: URL) => `
-# Created in the distant future (the year 2000) after
-# the robotic uprising of the mid 90's which wiped out all humans.
-
-User-agent: *
-Disallow: /cgi-bin/
 Sitemap: ${sitemapURL.href}
 `;
 
