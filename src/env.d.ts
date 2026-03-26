a59c6183dc852603114523fd55604eae6b082b09
Åzze <85607228+jere-a@users.noreply.github.com>
2026-03-26 12:14:31 +0200

    Defining blocking render for specifyng render blocking resource

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,8 @@
 /// <reference types="astro/client" />
 /// <reference path="./types/index.d.ts">
 
+declare namespace astroHTML.JSX {
+  interface ScriptHTMLAttributes {
+    blocking?: string;
+  }
+}
