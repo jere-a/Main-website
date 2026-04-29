d85ec3bd4cc82d529836e16fc9cc4e4a928c533a
Åzze <85607228+jere-a@users.noreply.github.com>
2026-04-29 11:22:58 +0300

    Adding more types checking for configs

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,6 @@
-const author = {
-  name: "equilibriumuk",
-  photo: "/users/",
-  bio: "Web Developer & SRE",
-  contacts: {
-    github: "jere-a",
-  },
-} as const;
-
-const urls = {
-  giturl: "https://github.com/jere-a/Main-website",
-} as const;
-
 export type SiteConfig = {
   host: string;
   url: string;
-  oldSite: string;
   title: string;
   Blogtitle: string;
   siteName: string;
@@ -26,21 +12,36 @@ export type SiteConfig = {
   yt_cid: string;
   posthog_id: string;
   mainLanguage: string;
-  author: typeof author;
-  urls: typeof urls;
+  author: {
+    contacts: {
+      github: string;
+    };
+  };
+  urls: {
+    giturl: string;
+  };
 };
 
-export const siteConfig = {
+const author = {
+  contacts: {
+    github: "jere-a",
+  },
+} as const;
+
+const urls = {
+  giturl: "https://github.com/jere-a/Main-website",
+} as const;
+
+export const siteConfig: SiteConfig = {
   host: "ozze.eu.org",
   url: "https://ozze.eu.org",
-  oldSite: "http://old.ozze.eu.org/index.html",
   title: "Åzze",
   Blogtitle: "Åzze's Blog",
   siteName: "",
   description: "Pieni nettisivuni.",
   rss_description: "Small Content Creator for the internet.",
   postsPerPage: 7,
-  postsRecent: 5,
+  postsRecent: 200,
   mainAuthor: "Jere E.L",
   yt_cid: "UCNAVV2j-Bmuu9ApfTYwYAeA",
   posthog_id: "phc_5MXCIWNtl5iS3fpCybKZjGJoe1RIoJlpHGBwfZgfUFF",
