74722959f5f2e391def6d404af33ab712089575b
Åzze <85607228+jere-a@users.noreply.github.com>
2026-05-07 13:39:18 +0300

    Adding konami code and @tanstack/hotkeys for it and other updates to packages

--- src/ts/global-code.ts
+++ src/ts/global-code.ts
@@ -1,3 +1,8 @@
+import {
+  checkHotkey,
+  createSequenceMatcher,
+  type HotkeySequence,
+} from "@tanstack/hotkeys";
 import { addEventListener, isHoliday } from "./global/index";
 
 const main = () => {
@@ -9,6 +14,32 @@ const main = () => {
     },
     "img, picture",
   );
+
+  const konamiCode: HotkeySequence = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "B",
+    "A",
+  ];
+
+  for (const hotkey in konamiCode) {
+    checkHotkey(hotkey);
+  }
+
+  const konami = createSequenceMatcher(konamiCode, { timeout: 2000 });
+
+  window.addEventListener("keydown", (event) => {
+    if (konami.match(event)) {
+      // biome-ignore lint/suspicious/noConsole: Konami code message for now for info if it's going to be enabled
+      console.log("Konami code activated.");
+    }
+  });
 
   window.addEventListener("vite:preloadError", () => {
     window.location.reload();
